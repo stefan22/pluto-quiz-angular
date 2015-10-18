af4de353559fc435131f39f92959abe8233f38ee
Stefan Lissia <stefan.emailacct@gmail.com>
2015-10-18 23:37:22 +0100

    fixes problem loading angular json data

--- js/script.js
+++ js/script.js
@@ -4,8 +4,15 @@
 
 	app.controller('QuizController',['$scope','$http','$sce', function($scope,$http,$sce) {
 
-		$http.get('quiz_data.json').then(function(quiz) {
-			$scope.myQuestions = quiz.data;
+		$scope.score = 0;
+		$scope.activeQuestion = -1;
+		$scope.activeQuestionAnswered = 0;
+		$scope.percentage = 0;
+		$scope.welcome = "hello all";
+
+		$http.get('js/quiz_data.json').then(function(quiz){
+			$scope.myQuestions = quiz.data; //all data inside myQuestions
+			$scope.totalQuestions = $scope.myQuestions.length;
 
 		});
 
