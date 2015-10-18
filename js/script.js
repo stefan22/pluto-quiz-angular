338486250e5a1875ef7cb75c20b0e9135d771f29
Stefan Lissia <stefan.emailacct@gmail.com>
2015-10-18 20:02:02 +0100

    loading json data

--- js/script.js
+++ js/script.js
@@ -4,7 +4,11 @@
 
 	app.controller('QuizController',['$scope','$http','$sce', function($scope,$http,$sce) {
 
-		
+		$http.get('quiz_data.json').then(function(quiz) {
+			$scope.myQuestions = quiz.data;
+
+		});
+
 
 	}]);
 
