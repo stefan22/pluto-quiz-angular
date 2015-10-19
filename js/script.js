1ff353f4489bc4f678d0230e32e94486bd739c71
Stefan Lissia <stefan.emailacct@gmail.com>
2015-10-19 20:21:00 +0100

    replaces progress bar divs with ng-repeat div

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
 		$scope.activeQuestion = -1;
 		$scope.activeQuestionAnswered = 0;
 		$scope.percentage = 0;
-		$scope.welcome = "hello all";
+		
 
 		$http.get('js/quiz_data.json').then(function(quiz){
 			$scope.myQuestions = quiz.data; //all data inside myQuestions
